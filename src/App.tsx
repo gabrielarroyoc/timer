3051f2baacf42038b5fba275451afa521199c3a6
gabrielarroyoc <gabrielarroyoc18@gmail.com>
2023-01-20 14:11:45 -0300

    convertendo para contextos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
-import { Router } from './Router'
-import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-      <Router />
+        <Router />
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
