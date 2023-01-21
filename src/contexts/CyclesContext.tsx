019c45d69ce29b638feb5c09c772d81785c2d1ee
gabrielarroyoc <gabrielarroyoc18@gmail.com>
2023-01-21 01:41:11 -0300

    reset do formulario

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -68,7 +68,7 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
     setAmountSecondsPassed(0);
-    // reset();
+   
   }
 
   function interruptCurrentCycle() {
